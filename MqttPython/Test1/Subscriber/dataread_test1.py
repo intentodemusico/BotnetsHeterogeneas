6bc1ce8aea60a1f6f6e2f0aca4d63f5cb64b2051
intentodemusico <44100370+intentodemusico@users.noreply.github.com>
2019-03-19 16:12:39 -0500

    Update dataread_test1.py

--- MqttPython/Test1/Subscriber/dataread_test1.py
+++ MqttPython/Test1/Subscriber/dataread_test1.py
@@ -19,7 +19,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect(broker,port)
-
+#hola
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
