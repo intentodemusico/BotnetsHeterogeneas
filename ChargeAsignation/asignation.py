210dd8438e371dbf0da0e012e0db8541748eae97
intentodemusico <44100370+intentodemusico@users.noreply.github.com>
2019-03-21 14:56:21 -0500

    Documents update

--- ChargeAsignation/asignation.py
+++ ChargeAsignation/asignation.py
@@ -1,4 +1,8 @@
-import mpt #Import measuring processing time
+import mpt as mpt
+def asignation(ndecimals):
+    time=mpt.calculateProcessingTime(ndecimals)
+    if (time<
+#Import measuring processing time
 # bla bla bla, importa el método para medición y luego bla bla para ver cuánta
 #carga asignamos
 #Se asigna como nodo de respaldo cuando su capacidad de procesamiento bla bla bla
