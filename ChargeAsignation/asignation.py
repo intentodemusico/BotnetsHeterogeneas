47d302189653c38b3b2d478e8923d42a997782ac
? <alberto.garcia.2016@upb.edu.co>
2019-04-11 10:29:20 -0500

    Deleted mpt files

--- ChargeAsignation/asignation.py
+++
@@ -1,9 +0,0 @@
-import mpt as mpt
-def asignation(ndecimals):
-    time=mpt.calculateProcessingTime(ndecimals)
-    if (time<
-#Import measuring processing time
-# bla bla bla, importa el método para medición y luego bla bla para ver cuánta
-#carga asignamos
-#Se asigna como nodo de respaldo cuando su capacidad de procesamiento bla bla bla
-#con una función que retorna un booleano
