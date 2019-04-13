16c45e690190c425c517816c49a160fe16537696
? <alberto.garcia.2016@upb.edu.co>
2019-04-13 00:25:06 -0500

    Update SubscriberHeartbeat.py
    Every 15 seconds subscrifer is looking for a heartbeat

--- MqttPython/Mqtt/PublisherSubscriber/SubscriberHeartbeat.py
+++ MqttPython/Mqtt/PublisherSubscriber/SubscriberHeartbeat.py
@@ -1,8 +1,9 @@
 import paho.mqtt.client as mqtt
+import time
 broker = "iot.eclipse.org"
 port = 1883
 global received
-received=0
+received=-1
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -16,23 +17,32 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
-    print(msg)
+    print(" ")
     global received
     received=1
-    print("Received:",received)
+    client.loop_stop()
     
-    
+
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect(broker,port)
-#hola
+
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
 
+#Runs until it receives a message
+
 client.loop_start()
-while(received==0):
-    pass
-client.loop_stop()
+while(True):
+    if(received==0):
+        client.loop_stop()
+        print("Vemos, no hay nadie vivo")
+        break
+    else:
+        received=0
+        client.loop(15)
+        time.sleep(15)
+        print("Pasaron 15 segundos")
