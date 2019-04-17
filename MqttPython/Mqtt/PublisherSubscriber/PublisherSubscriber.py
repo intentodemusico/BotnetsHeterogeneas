70b41ffba1ccfd3f1834f4c1d7a2f847b66a55ed
? <alberto.garcia.2016@upb.edu.co>
2019-04-17 12:40:58 -0500

    Update PublisherSubscriber.py
    Node leadership assignation improved, id substraction  when no received messages is working.
    When master node dies, all nodes substract 1 from their id; new master node should have id 1.

--- MqttPython/Mqtt/PublisherSubscriber/PublisherSubscriber.py
+++ MqttPython/Mqtt/PublisherSubscriber/PublisherSubscriber.py
@@ -8,6 +8,7 @@ global lastId
 received=-1
 lastId=-555
 nodeId=-1
+restas=0
 
 heartbeat="Node "+str(nodeId)+" alive"
 
@@ -57,6 +58,7 @@ client.connect(broker,port)
 
 #Runs until it receives a message
 
+#
 startTime=time.time()
 while(True):
     client.loop(.1)
@@ -64,20 +66,41 @@ while(True):
     if(int((time.time()-startTime)%60%14)==0):
         time.sleep(1)## 15 seconds elapsed
         #print("15 seconds elapsed")
-        if(nodeId==-1 and received==1):   
-            setNodeId(lastId+1)
-        if(received==0):
+
+        
+        # if(nodeId==-1 and received==1):   
+        #     setNodeId(lastId+1)
+        # if(received==0):
+        #     print("No alive nodes, i'll be the master one")
+        #     #client.loop_stop()
+        #     break
+        # else:
+        #     received=0
+
+        if(received==1):
+            print("Id",nodeId,"lastId",lastId)
+            if(nodeId<=-1):  
+                setNodeId(lastId+1)
+        elif(nodeId<=1 and received==0): #Fixiable
             print("No alive nodes, i'll be the master one")
             #client.loop_stop()
             break
         else:
-            received=0
+            nodeId-=1
+            restas+=1
+            print("NodeId--",nodeId,"restas",restas)
+        received=0
 
-                
-if(nodeId==-1):
+
+#Node take leadership of the botnet                
+if(nodeId<=-1):
     lastId=0
+else:
+    restas+=1
+    lastId-=restas
 nodeId=0
 startTime=time.time()
+
 while(nodeId==0):
     client.loop(.1)
     if(int((time.time()-startTime)%60%10)==0):
